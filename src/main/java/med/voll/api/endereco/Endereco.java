c56a8c912628ef2935d29f03b0b06b67257edb9e
Thiago <thiagofelipew80@gmail.com>
2023-04-22 22:50:24 -0300

    API REST criada para aprendizado do framework Spring Boot

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,55 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+
+    public Endereco(DadosEndereco dados) {
+        this.logradouro = dados.logradouro();
+        this.bairro = dados.bairro();
+        this.cep = dados.cep();
+        this.uf = dados.uf();
+        this.cidade = dados.cidade();
+        this.numero = dados.numero();
+        this.complemento = dados.complemento();
+    }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if(logradouro != null){
+            this.logradouro = dados.logradouro();
+        }
+        if(bairro != null){
+            this.bairro = dados.bairro();
+        }
+        if(cep != null){
+            this.cep = dados.cep();
+        }
+        if(uf != null) {
+            this.uf = dados.uf();
+        }
+        if(cidade != null){
+            this.cidade = dados.cidade();
+        }
+        if(numero != null){
+            this.numero = dados.numero();
+        }
+        if(complemento != null){
+            this.complemento = dados.complemento();
+        }
+    }
+}
