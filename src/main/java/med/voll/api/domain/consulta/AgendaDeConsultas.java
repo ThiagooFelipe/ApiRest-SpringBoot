799fb7b391f4f956559502d97631f95f860d65be
Thiago <thiagofelipew80@gmail.com>
2023-06-01 22:39:43 -0300

    Alteracoes na funcionalidade de agendamento consultas e inclusao de nova funcionalidade para cancelamento de consultas, atualizacao no banco de dados tabela consultas para adicao da coluna motivo-cancelamento

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,7 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +21,39 @@ public class AgendaDeConsultas {
 
     public void agendar(DadosAgendamentoConsulta dados){
 
-        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
-        var medico = medicoRepository.findById(dados.idMedico()).get();
+        if (!pacienteRepository.existsById(dados.idPaciente())){
+            throw new ValidacaoException("Id do paciente informado não existe");
+        }
 
-        var consulta = new Consulta(null, medico, paciente, dados.data());
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())){
+            throw new ValidacaoException("Id do medico informado não existe");
+        }
+
+        var paciente = pacienteRepository.getReferenceById(dados.idPaciente());
+        var medico = escolherMedico(dados);
+
+        var consulta = new Consulta(null, medico, paciente, dados.data(), null);
         consultaRepository.save(consulta);
+    }
+
+    private Medico escolherMedico(DadosAgendamentoConsulta dados) {
+        if (dados.idMedico() != null){
+            return medicoRepository.getReferenceById(dados.idMedico());
+        }
+
+        if (dados.especialidade() == null){
+            throw new ValidacaoException("Especialidade obrigatoria caso médico não for escolhido");
+        }
+
+        return medicoRepository.escolherMedicoDisponivelAleatorio(dados.especialidade(), dados.data());
+    }
+
+    public void cancelar(DadosCancelamentoConsulta dados) {
+        if (!consultaRepository.existsById(dados.idConsulta())) {
+            throw new ValidacaoException("Id da consulta informado não existe!");
+        }
+
+        var consulta = consultaRepository.getReferenceById(dados.idConsulta());
+        consulta.cancelar(dados.motivo());
     }
 }
